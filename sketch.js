4d18f954b225f7d35e6df667378efedf1de70b13
themooses <wilhelm.rauston@gmail.com>
2023-02-03 14:39:18 +0100

    Mesh Layout

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-let X;
-let Y;
-let Z;
-let centerX;
-let centerY;
-let centerZ;
-let h = 20;
+
 
 function setup(){
-  createCanvas(800, 800, WEBGL);
-  camera(Z, Y, Z, centerX, centerY, centerZ, 0, 100, 0);
+  createCanvas(1920, 1080, WEBGL);
+  camera(180, -800, 200, 200, 0 ,200)
+  ambientLight(255); // white light
 }
 
 function draw(){
 
-  background(220);
-
+  background(221);
   orbitControl();
-  rotateY(0.5);
-  for (let x=0; x < width; x +=20){
-    for (let z=0; z < height; z +=20){
-      push();
+
+
+  for (let x=0; x < 400; x +=20){
+    for (let z=0; z < 400; z +=20){
+      if (x == 0 | z == 0) {
+        push();
+        translate(x, -3.25, z);
+        box(18.5, 25, 18.5);
+        pop();
+
+      }
+      else if (x == 380 && z < 400) {
+        push();
+        translate(x, -3.25, z);
+        box(18.5, 25, 18.5);
+        pop();
+      }
+      else if (z == 380 && x < 400) {
+        push();
+        translate(x, -3.25, z);
+        box(18.5, 25, 18.5);
+        pop();
+      }
+      else {
+        push();
       // ground plane is XZ, not XY (front plane)
       translate(x, 0, z);
-      box(20);
+      box(18.5);
       pop();
+      }
     }
   }
 
